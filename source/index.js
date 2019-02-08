610e637bf16fac0631c2f217432e19fde0bcf192
scann <apple.eater01@gmail.com>
2019-02-08 16:55:38 +0200

    Redux store connected to App

--- source/index.js
+++ source/index.js
@@ -1,11 +1,17 @@
 // Core
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 // Instruments
 import './theme/init';
+import { store } from './init/store';
 
 // App
 import App from './pages/App';
 
-render(<App />, document.getElementById('app'));
+render(
+    <Provider store = { store }>
+        <App />
+    </Provider>, document.getElementById('app')
+);
