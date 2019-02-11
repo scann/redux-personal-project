73f9d3f28e8386f4459e145016bbc33ec4b1eb2e
scann <apple.eater01@gmail.com>
2019-02-11 15:38:52 +0200

    uiReducer added to rootReducer

--- source/init/rootReducer.js
+++ source/init/rootReducer.js
@@ -4,8 +4,10 @@ import { combineReducers } from 'redux';
 //Reducers
 import { tasksReducer as tasks } from '../bus/tasks/reducer';
 import { formReducer as form } from '../bus/form/reducer';
+import { uiReducer as ui } from '../bus/ui/reducer';
 
 export const rootReducer = combineReducers({
     tasks,
     form,
+    ui,
 });
