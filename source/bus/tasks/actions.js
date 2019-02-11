f98941194da260e3210b083871bd305d7e3240b1
scann <apple.eater01@gmail.com>
2019-02-11 12:57:33 +0200

    createTask - new types, actions, reducer

--- source/bus/tasks/actions.js
+++ source/bus/tasks/actions.js
@@ -9,11 +9,23 @@ export const tasksActions = {
             payload: tasks,
         };
     },
+    createTask: (message) => {
+        return {
+            type:    types.CREATE_TASK,
+            payload: message,
+        };
+    },
 
     //Async
     fetchTasksAsync: () => {
         return {
             type: types.FETCH_TASKS_ASYNC,
+        };
+    },
+    createTaskAsync: (message) => {
+        return {
+            type:    types.CREATE_TASK_ASYNC,
+            payload: message,
         };
     },
 };
--- source/bus/tasks/reducer.js
+++ source/bus/tasks/reducer.js
@@ -10,6 +10,8 @@ export const tasksReducer = (state = initialState, action) => {
     switch (action.type) {
         case types.FILL_TASKS:
             return fromJS(action.payload);
+        case types.CREATE_TASK:
+            return state.unshift(fromJS(action.payload));
         default:
             return state;
     }
