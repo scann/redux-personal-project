aff4bd1f7947f75c5422d1760bf175c2684d5425
scann <apple.eater01@gmail.com>
2019-02-11 14:38:19 +0200

    removeTask worker, watcher and index update

--- source/bus/tasks/saga/watchers.js
+++ source/bus/tasks/saga/watchers.js
@@ -5,7 +5,7 @@ import { takeEvery, all, call } from 'redux-saga/effects';
 import { types } from '../types';
 
 //Workers
-import { fetchTasks, createTask } from './workers';
+import { fetchTasks, createTask, removeTask } from './workers';
 
 export function* watchFetchTasks () {
     yield takeEvery(types.FETCH_TASKS_ASYNC, fetchTasks);
@@ -15,6 +15,10 @@ export function* watchCreateTask () {
     yield takeEvery(types.CREATE_TASK_ASYNC, createTask);
 }
 
+export function* watchRemoveTask () {
+    yield takeEvery(types.REMOVE_TASK_ASYNC, removeTask);
+}
+
 export function* watchTasks () {
-    yield all([call(watchFetchTasks), call(watchCreateTask)]);
+    yield all([call(watchFetchTasks), call(watchCreateTask), call(watchRemoveTask)]);
 }
--- source/bus/tasks/saga/workers/index.js
+++ source/bus/tasks/saga/workers/index.js
@@ -1,2 +1,3 @@
 export { fetchTasks } from './fetchTasks';
 export { createTask } from './createTask';
+export { removeTask } from './removeTask';
