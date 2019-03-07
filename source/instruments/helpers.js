4dae991c1a4c723e5e47df79c041f223417203a7
scann <apple.eater01@gmail.com>
2019-03-07 11:38:01 +0200

    Helper functions revisited for using immutable data structures

--- source/instruments/helpers.js
+++ source/instruments/helpers.js
@@ -20,15 +20,15 @@ export const sortTasksByDate = (tasks) => {
 };
 
 export const sortTasksByGroup = (tasks) => {
-    const favorite = tasks.filter((task) => task.favorite && !task.completed);
-    const usual = tasks.filter((task) => !task.favorite && !task.completed);
-    const completed = sortTasksByDate(tasks.filter((task) => task.completed));
+    const favorite = tasks.filter((task) => task.get('favorite') && !task.get('completed'));
+    const usual = tasks.filter((task) => !task.get('favorite') && !task.get('completed'));
+    const completed = sortTasksByDate(tasks.filter((task) => task.get('completed')));
 
     const sortedCompleted = [
         ...completed.sort((task1, task2) => {
-            if (task1.favorite && !task2.favorite) {
+            if (task1.get('favorite') && !task2.get('favorite')) {
                 return -1;
-            } else if (!task1.favorite && task2.favorite) {
+            } else if (!task1.get('favorite') && task2.get('favorite')) {
                 return 1;
             }
 
