550742420091ad5b2fc52e08a83c8bb016b0844b
scann <apple.eater01@gmail.com>
2019-03-12 17:34:51 +0200

    Tests for fetchTasks saga refactoring with cloneableGenerator

--- source/bus/tasks/__tests__/fetchTasks.saga.js
+++ source/bus/tasks/__tests__/fetchTasks.saga.js
@@ -1,29 +1,76 @@
 //Core
-import { put, apply } from 'redux-saga/effects';
-import { expectSaga } from 'redux-saga-test-plan';
+import { put, apply } from "redux-saga/effects";
+import { cloneableGenerator } from "redux-saga/utils";
 
 //Instruments
 import { api } from '../../../REST';
-import { tasksActions } from '../actions';
 import { uiActions } from '../../ui/actions';
+import { tasksActions } from '../actions';
 import { fetchTasks } from '../saga/workers';
 
-describe('fetchTasks saga:', () => {
-    test('should complete a 200 status response scenario', async () => {
-        await expectSaga(fetchTasks)
-            .put(uiActions.startSpinning())
-            .provide([[apply(api, api.tasks.fetch), __.fetchResponseSuccess]])
-            .put(tasksActions.fillTasks(__.tasksList))
-            .put(uiActions.stopSpinning())
-            .run();
+const fetchTasksAction = tasksActions.fetchTasksAsync(__.tasksList);
+
+const saga = cloneableGenerator(fetchTasks)(fetchTasksAction);
+let clone = null;
+
+describe('fetchTasks saga', () => {
+    describe('should pass until response received', () => {
+        test('should dispatch "startSpinning" action', () => {
+            expect(saga.next().value).toEqual(put(uiActions.startSpinning()));
+        });
+
+        test('should call a fetch request', () => {
+            expect(saga.next().value).toEqual(
+                apply(api, api.tasks.fetch)
+            );
+            clone = saga.clone();
+        });
     });
 
-    test('should complete a 400 status response scenario', async () => {
-        await expectSaga(fetchTasks)
-            .put(uiActions.startSpinning())
-            .provide([[apply(api, api.tasks.fetch), __.fetchResponseFail400]])
-            .put(uiActions.emitError(__.emitError, 'fetchTasks worker'))
-            .put(uiActions.stopSpinning())
-            .run();
+    describe('should handle a 400 status response', () => {
+        test('a fetch request should return 400 status response', () => {
+            expect(clone.next(__.fetchResponseFail400).value).toEqual(
+                apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+            );
+        });
+
+        test('should contain a response data object', () => {
+            expect(clone.next(__.responseDataFail).value).toEqual(
+                put(uiActions.emitError(__.emitError, 'fetchTasks worker'))
+            );
+        });
+
+        test('should dispatch "stopSpinning" action', () => {
+            expect(clone.next().value).toEqual(put(uiActions.stopSpinning()));
+        });
+
+        test('should finish', () => {
+            expect(clone.next().done).toBe(true);
+        });
     });
+
+    describe('should handle a 200 status response', () => {
+        test('a fetch request should return a 200 status response data object', () => {
+            expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+                apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+            );
+        });
+
+        test('should dispatch "fillTasks" action', () => {
+            expect(saga.next(__.responseDataSuccess).value).toMatchSnapshot();
+        });
+
+        test('should dispatch "reset" action', () => {
+            expect(saga.next(__.responseDataSuccess).value).toMatchSnapshot();
+        });
+
+        test('should dispatch "stopSpinning" action', () => {
+            expect(saga.next().value).toMatchSnapshot();
+        });
+
+        test('should finish', () => {
+            expect(saga.next().done).toBe(true);
+        });
+    });
+
 });
