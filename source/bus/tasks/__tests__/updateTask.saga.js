47576c053f2f59f838f5e0f8c9d213cf9597f99b
scann <apple.eater01@gmail.com>
2019-03-14 23:31:33 +0200

    updateTask saga tests with redux-saga-test-plan

--- source/bus/tasks/__tests__/updateTask.saga.js
+++ source/bus/tasks/__tests__/updateTask.saga.js
@@ -10,19 +10,19 @@ import { updateTask } from '../saga/workers';
 
 describe('updateTask saga:', () => {
     test('should complete a 200 status response scenario', async () => {
-        await expectSaga(updateTask, __.task)
+        await expectSaga(updateTask, { payload: __.task })
             .put(uiActions.startSpinning())
-            .provide([[apply(api, api.tasks.update, [__.task]), __.fetchResponseSuccessUp]])
-            //.put(tasksActions.updateTask(__.task))
+            .provide([[apply(api, api.tasks.update, [__.task]), __.fetchResponseSuccessUpdate]])
+            .put(tasksActions.updateTask(__.task))
             .put(uiActions.stopSpinning())
             .run();
     });
 
     test('should complete a 400 status response scenario', async () => {
-        await expectSaga(updateTask, __.task)
+        await expectSaga(updateTask, { payload: __.task })
             .put(uiActions.startSpinning())
             .provide([[apply(api, api.tasks.update, [__.task]), __.fetchResponseFail400]])
-            //.put(uiActions.emitError(__.emitError, 'updateTasks worker'))
+            .put(uiActions.emitError(__.error, 'updateTask worker'))
             .put(uiActions.stopSpinning())
             .run();
     });
